feaea938dbec683e17bc640ee0a535c93c33c45b
Ahn Kiwook <ahnkiwook@gmail.com>
2022-10-27 22:06:21 +0900

    Assembling Hangul experimental

--- app/src/main/kotlin/pe/aioo/openmoa/OpenMoaIME.kt
+++ app/src/main/kotlin/pe/aioo/openmoa/OpenMoaIME.kt
@@ -12,27 +12,87 @@ import android.view.WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
 import android.view.inputmethod.EditorInfo
 import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.github.kimkevin.hangulparser.HangulParser
+import com.github.kimkevin.hangulparser.HangulParserException
 
 class OpenMoaIME : InputMethodService() {
 
     private lateinit var broadcastReceiver: BroadcastReceiver
+    private val jamoList: ArrayList<String> = arrayListOf()
 
     override fun onCreate() {
         super.onCreate()
         broadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
                 val key = intent.getStringExtra(EXTRA_NAME) ?: return
-                if (key.length == 1) {
-                    currentInputConnection.commitText(key, 1)
-                } else {
+                // Process for special key
+                if (key.length > 1) {
                     when (key) {
-                        SpecialKey.BACKSPACE.value -> currentInputConnection.deleteSurroundingText(
-                            1, 0
-                        )
+                        SpecialKey.BACKSPACE.value -> {
+                            if (jamoList.size > 0) {
+                                jamoList.removeLast()
+                                val assembled = try {
+                                    HangulParser.assemble(jamoList)
+                                } catch (_: HangulParserException) {
+                                    jamoList.joinToString("")
+                                }
+                                currentInputConnection.setComposingText(assembled, 1)
+                            } else {
+                                currentInputConnection.deleteSurroundingText(1, 0)
+                            }
+                        }
                         SpecialKey.ENTER.value -> currentInputConnection.performEditorAction(
                             EditorInfo.IME_ACTION_GO
                         )
                     }
+                    return
+                }
+                // Process for non-Hangul key
+                if (!key.matches(Regex("^[ㄱ-ㅎㅏ-ㅣ]$"))) {
+                    if (jamoList.size > 0) {
+                        val assembled = try {
+                            HangulParser.assemble(jamoList)
+                        } catch (_: HangulParserException) {
+                            jamoList.joinToString("")
+                        }
+                        currentInputConnection.commitText(assembled, 1)
+                        jamoList.clear()
+                    }
+                    currentInputConnection.commitText(key, 1)
+                    return
+                }
+                // Process for Hangul key
+                jamoList.add(key)
+                if (jamoList.size == 1) {
+                    currentInputConnection.setComposingText(key, 1)
+                    return
+                }
+                try {
+                    val assembled = HangulParser.assemble(jamoList)
+                    if (assembled.length > 1) {
+                        val composed = assembled.substring(0, 1)
+                        currentInputConnection.commitText(composed, 1)
+                        currentInputConnection.setComposingText(
+                            assembled.substring(1, 2), 1
+                        )
+                        for (i in 0 until HangulParser.disassemble(composed).size) {
+                            jamoList.removeFirst()
+                        }
+                        return
+                    }
+                    currentInputConnection.setComposingText(assembled, 1)
+                } catch (e: HangulParserException) {
+                    val prevJamoList = jamoList.subList(0, jamoList.size - 1)
+                    val assembled = try {
+                        HangulParser.assemble(prevJamoList)
+                    } catch (_: HangulParserException) {
+                        prevJamoList.joinToString("")
+                    }
+                    for (i in 0 until prevJamoList.size) {
+                        jamoList.removeFirst()
+                    }
+                    currentInputConnection.commitText(assembled, 1)
+                    currentInputConnection.setComposingText(jamoList.first(), 1)
                 }
             }
         }
