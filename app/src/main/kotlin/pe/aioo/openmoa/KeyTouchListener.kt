79a5100623f6bb3fbe89b23c5a2a885a7ecdf5ae
Ahn Kiwook <ahnkiwook@gmail.com>
2022-10-29 23:50:50 +0900

    Moeum combination from dragging experimental

--- app/src/main/kotlin/pe/aioo/openmoa/KeyTouchListener.kt
+++ app/src/main/kotlin/pe/aioo/openmoa/KeyTouchListener.kt
@@ -30,6 +30,7 @@ class KeyTouchListener(
 
     private var startX: Float = 0f
     private var startY: Float = 0f
+    private val moeumList = arrayListOf<String>()
 
     override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
         when (motionEvent.action) {
@@ -37,26 +38,96 @@ class KeyTouchListener(
                 view.background = backgrounds[0]
                 startX = motionEvent.x
                 startY = motionEvent.y
+                moeumList.clear()
             }
-            MotionEvent.ACTION_UP -> {
-                val endX = motionEvent.x
-                val endY = motionEvent.y
-                val distance = sqrt((endX - startX).pow(2) + (endY - startY).pow(2))
-                val degree = (atan2(endY - startY, endX - startX) * 180f) / PI
-                sendKey(key)
+            MotionEvent.ACTION_MOVE -> {
+                val currentX = motionEvent.x
+                val currentY = motionEvent.y
+                val distance = sqrt(
+                    (currentX - startX).pow(2) + (currentY - startY).pow(2)
+                )
                 if (distance > 25f) {
+                    val degree = (atan2(currentY - startY, currentX - startX) * 180f) / PI
+                    startX = currentX
+                    startY = currentY
                     if (abs(degree) < 22.5f) {
-                        sendKey("ㅏ")
+                        moeumList.add("ㅏ")
                     } else if (abs(degree) < 67.5f) {
-                        sendKey(if (degree > 0) "ㅡ" else "ㅣ")
+                        moeumList.add(if (degree > 0) "ㅡ" else "ㅣ")
                     } else if (abs(degree) < 112.5f) {
-                        sendKey(if (degree > 0) "ㅜ" else "ㅗ")
+                        moeumList.add(if (degree > 0) "ㅜ" else "ㅗ")
                     } else if (abs(degree) < 157.5f) {
-                        sendKey(if (degree > 0) "ㅡ" else "ㅣ")
+                        moeumList.add(if (degree > 0) "ㅡ" else "ㅣ")
                     } else {
-                        sendKey("ㅓ")
+                        moeumList.add("ㅓ")
                     }
                 }
+            }
+            MotionEvent.ACTION_UP -> {
+                sendKey(key)
+                var moeum: String? = null
+                for (nextMoeum in moeumList) {
+                    moeum = when (moeum) {
+                        "ㅏ" -> when (nextMoeum) {
+                            "ㅓ" -> "ㅐ"
+                            else -> moeum
+                        }
+                        "ㅐ" -> when (nextMoeum) {
+                            "ㅏ" -> "ㅑ"
+                            else -> moeum
+                        }
+                        "ㅑ" -> when (nextMoeum) {
+                            "ㅓ" -> "ㅒ"
+                            else -> moeum
+                        }
+                        "ㅓ" -> when (nextMoeum) {
+                            "ㅏ" -> "ㅔ"
+                            else -> moeum
+                        }
+                        "ㅔ" -> when (nextMoeum) {
+                            "ㅓ" -> "ㅕ"
+                            else -> moeum
+                        }
+                        "ㅕ" -> when (nextMoeum) {
+                            "ㅏ" -> "ㅖ"
+                            else -> moeum
+                        }
+                        "ㅗ" -> when (nextMoeum) {
+                            "ㅏ" -> "ㅘ"
+                            "ㅜ" -> "ㅚ"
+                            else -> moeum
+                        }
+                        "ㅘ" -> when (nextMoeum) {
+                            "ㅓ" -> "ㅙ"
+                            else -> moeum
+                        }
+                        "ㅚ" -> when (nextMoeum) {
+                            "ㅗ" -> "ㅛ"
+                            else -> moeum
+                        }
+                        "ㅜ" -> when (nextMoeum) {
+                            "ㅓ" -> "ㅝ"
+                            "ㅗ" -> "ㅟ"
+                            else -> moeum
+                        }
+                        "ㅝ" -> when (nextMoeum) {
+                            "ㅏ" -> "ㅞ"
+                            else -> moeum
+                        }
+                        "ㅟ" -> when (nextMoeum) {
+                            "ㅜ" -> "ㅠ"
+                            else -> moeum
+                        }
+                        "ㅡ" -> when (nextMoeum) {
+                            "ㅣ" -> "ㅢ"
+                            else -> "ㅡ"
+                        }
+                        "ㅣ" -> "ㅣ"
+                        null -> nextMoeum
+                        else -> moeum
+                    }
+                }
+                moeum?.let { sendKey(it) }
                 view.background = backgrounds[1]
                 view.performClick()
             }
